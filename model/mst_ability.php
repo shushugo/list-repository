7e31fcaafb0370cce25768cb132011f679ae662c
shugo <shugo.t114@icloud.com>
2022-08-16 19:16:34 +0900

    commit

---
+++ model/mst_ability.php
@@ -0,0 +1,60 @@
+<?php 
+
+class mst_ability {
+  
+  //定数
+  define('DSN' , 'mysql:dbname=test;host=mysql;charset=utf8mb4');
+  define('USER' , 'test');
+  define('PASSWORD' , 'test');
+
+  //データ取得
+  public function GetData {
+    try {
+      $select = 'SELECT * FROM mst_ability';
+      $dbh = new PDO(DSN, USER, PASSWORD, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      ]);
+      $sql = $select.$where.$order;
+      $stmt = $dbh->prepare($sql);
+      $stmt->execute();
+      return $stmt->fetchAll();
+    } catch (PDOException $e) {
+      echo 'エラーメッセージ:「データが存在しません」: ' . $e->getMessage();
+    }
+  }
+
+  //データ挿入
+  public function Insert {
+    try {
+      $insert = 'INSERT INTO mst_ability';
+      $dbh = new PDO(DSN, USER, PASSWORD, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      ]);
+      $sql = $insert.$where.$order;
+      $stmt = $dbh->prepare($sql);
+      $stmt->execute();
+      return $stmt->fetchAll();
+    } catch (PDOException $e) {
+      echo 'エラーメッセージ:「データが存在しません」: ' . $e->getMessage();
+    }
+  }
+
+  //データ更新
+  public function Update {
+    try {
+      $insert = 'UPDATE mst_ability';
+      $dbh = new PDO(DSN, USER, PASSWORD, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      ]);
+      $sql = $insert.$where.$order;
+      $stmt = $dbh->prepare($sql);
+      $stmt->execute();
+      return $stmt->fetchAll();
+    } catch (PDOException $e) {
+      echo 'エラーメッセージ:「データが存在しません」: ' . $e->getMessage();
+    }
+  }
+
+}
+
+?>
