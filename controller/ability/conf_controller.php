8d40ca2bc3d407bb9ee730da590e39ddea15ebd9
shugo <shugo.t114@icloud.com>
2022-08-22 19:44:22 +0900

    能力登録確認

--- controller/ability/conf_controller.php
+++ controller/ability/conf_controller.php
@@ -8,11 +8,43 @@ class conf_controller extends controller {
 
     session_start();
 
-    
+    $H = [
+      'register' => [
+        'ability_cd' => '',
+        'ability_name' => '',
+        'ability_name_kana' => ''
+      ],
+      'item' => [
+        'ability_cd' => '',
+        'ability_name' => '',
+        'ability_name_kana' => ''
+      ]
+    ];
 
+    $H['c'] = filter_input(INPUT_GET, 'c');
 
+    //能力コードがある場合は能力マスタからデータを取得し、値を格納する
+    if ($H['c']) {
+      $H['register'] = $mst_ability->GetData(['ability_cd' => $H['c']]);
 
+      //データを取得できないと能力一覧画面に移動
+      if (empty($H['register'])) {
+        header( "Location: index.php" );
+	      exit;
+      }
 
+      //能力登録用セッションに取得したデータを格納する
+      $_SESSION['ability']['register'] = $H['register'];
+
+      //戻るボタンの戻り先を能力一覧にする
+    }
+
+    //能力登録セッションに値がある場合はその値を表示する
+    if (isset($_SESSION['ability']['register'])) {
+      foreach ($H['item'] as $key => $value) {
+        $H['register'][$key] = $_SESSION['ability']['register'][$key];
+      }
+    }
 
     return $H;
   }
--- view/ability/conf.php
+++ view/ability/conf.php
@@ -11,24 +11,26 @@ $H = $controller->Load();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>能力登録確認</title>
+  <title>能力<?= (isset($H['c'])) ? '削除' : '登録'; ?>確認</title>
 </head>
 
 <body>
   <div>
-    <h1>能力登録確認</h1>
+    <h1>能力<?= (isset($H['c'])) ? '削除' : '登録'; ?>確認</h1>
   </div>
 
   <div>
     能力コード
+    <?= $H['register']['ability_cd'] ?><br>
 
     能力名
+    <?= $H['register']['ability_name'] ?><br>
 
     能力名カナ
+    <?= $H['register']['ability_name_kana'] ?><br>
   </div>
 
-  <a href="index.php">戻る</a>
-  <form method="POST">
-    <input type="submit" value="登録" name="btn_comp">
-  </form>
+    <a href= <?= (isset($H['c'])) ? 'index.php' : 'edit.php'; ?>>戻る</a>
+
+    <a href='comp.php'><?= (isset($H['c'])) ? '削除' : '登録'; ?></a>
 </body>
