8172c2e9bca5a971263b7d0354540bafeb1c5863
shugo <shugo.t114@icloud.com>
2022-09-02 16:40:41 +0900

    ページ用のファイルの追加

---
+++ library/page.php
@@ -0,0 +1,13 @@
+<?php 
+class page_menu {
+  public function Load($p, $maxpage) {
+    if ($p != 1) {
+      echo "<a href=\"index.php?p=".$p - 1 ."\">前のページ</a>";
+    }
+    
+    if ($p != $maxpage) {
+      echo "<a href=\"index.php?p=".$p + 1 ."\">次のページ</a>";
+    }
+  }
+}
+?>
