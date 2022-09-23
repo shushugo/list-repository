0a618e71959936926baa85a48cc84eda26b0ea95
shugo <shugo.t114@icloud.com>
2022-09-23 18:55:08 +0900

    都道府県登録完了画面の作成

--- prefecture/comp.php
+++ prefecture/comp.php
@@ -2,41 +2,51 @@
 session_start();
 
 //親クラスを読み込む
-require_once "../library/controller.php";
+require_once '../library/controller.php';
 
 class CompController extends Controller {
 
   public function load() {
     //モデルの読み込み
-    require_once "../library/SQL.php";
-    require_once "model/mst_prefectures.php";
-    $mst_prefectures = new MstPrefectures;
+    require_once '../library/SQL.php';
+    require_once 'model/mst_prefectures.php';
+    $mst_prfectures = new MstPrefectures;
 
-    $data = $_SESSION['prefecture']['register'];
+    $data = $this->getSessionPrefecture('register');
+
+    //都道府県更新のセッションの値を格納
+    $H['delete'] = $this->getSessionPrefecture('delete');
+
+    //都道府県更新のセッションの値を格納
+    $H['update'] = $this->getSessionPrefecture('update');
 
     //都道府県登録用セッションに値がある場合
     if ($data) {
-      if ($this->getGetParams('d')) {
+      if ($H['delete']) {
         //データ削除
         $H['crud'] = '削除';
-        $H['res'] = $mst_prefectures->delete($data, 'mst_prefecture');
-      } else if ($this->getGetParams('u')) {
+        $H['res'] = $mst_prfectures->delete($data);
+      } else if ($H['update']) {
         //データ更新
         $H['crud'] = '更新';
-        $H['res'] = $mst_prefectures->update($data, $data['prefecture_cd'], 'mst_prefecture');
+        $H['res'] = $mst_prfectures->update($data, $data['prefecture_cd']);
       } else {
         //データ追加
         $H['crud'] = '追加';
-        $H['res'] = $mst_prefectures->insert($data, 'mst_prefecture');
+        $H['res'] = $mst_prfectures->insert($data);
       }
     } else {
       $this->redirect("index.php");
     }
 
-    //都道府県登録用セッションを破棄する
-    unset($_SESSION['prefecture']['register']);
+    //都道府県登録用のセッションを破棄する
+    $this->clearSessionPrefecture('register');
+    //更新用のセッションを破棄する
+    $this->clearSessionPrefecture('update');
+    //削除用のセッションを破棄する
+    $this->clearSessionPrefecture('delete');
 
-    return $this->arrayMapH($H);
+    $this->buffer('../prefecture/view/comp_view.php',$H, '');
   }
 
 }
@@ -44,16 +54,6 @@ class CompController extends Controller {
 //comp_controllerクラスのインスタンス化
 $controller = new CompController;
 //comp_contorllerクラスのLoad関数を呼び出す
-$H = $controller->load();
-
-//記録開始
-ob_start();
-//viewファイルを読み込む
-require_once "view/comp_view.php";
-//記録結果を$bufferに代入
-$buffer = ob_get_contents();
-//記録終了
-ob_end_clean();
-echo $buffer;
+$controller->load();
 
 ?>
